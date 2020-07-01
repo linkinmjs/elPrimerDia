ce7e869accdc14f809e55c1333e45ce0e41cc648
Mauri <mauricio.stampella@25watts.com.ar>
2020-07-01 12:30:33 -0300

    aAñadiendo Hotkey

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React,{Fragment, useState, useEffect} from 'react';
 import Calendar from './components/Calendar';
 import Debug from './components/Debug';
-import axios from 'axios';
 
 import './App.css';
 
@@ -9,6 +8,7 @@ function App() {
 
   const [countryName, setCountryName] = useState({});
   const [countryCode, setCountryCode] = useState({});
+
 
   return (
     <Fragment>
